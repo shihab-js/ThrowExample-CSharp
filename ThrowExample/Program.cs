6c679d9840903f4afa16f57e560c862037ad6135
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-24 11:50:25 +0600

    Program completed....

--- ThrowExample/Program.cs
+++ ThrowExample/Program.cs
@@ -6,7 +6,34 @@ namespace ThrowExample
     {
         static void Main(string[] args)
         {
+            Student std = null;
+
+            try {
+
+                PrintStudentName(std);
             
+            }catch(Exception ex) {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        //create a method to print student name
+        public static void PrintStudentName(Student std)
+        {
+            if(std == null)
+            {
+                throw new NullReferenceException("Student object is null.");
+            }
+            Console.WriteLine(std.StudentName);
         }
     }
+}
+
+//create a Student Class
+public class Student
+{
+    public string StudentName { get; set; }
 }
